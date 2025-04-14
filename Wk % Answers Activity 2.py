3ef7d0334a58075340dad82dd08fbfa09d5c70fc
BAZ1995 <basilokata@gmail.com>
2025-04-14 16:08:33 +0300

    Create Wk % Answers Activity 2.py

---
+++ Wk % Answers Activity 2.py
@@ -0,0 +1,23 @@
+# Base Class
+class Vehicle:
+    def move(self):
+        print("Vehicle is moving...")
+
+# Subclasses
+class Car(Vehicle):
+    def move(self):
+        print("Driving 🚗")
+
+class Plane(Vehicle):
+    def move(self):
+        print("Flying ✈️")
+
+class Boat(Vehicle):
+    def move(self):
+        print("Sailing 🚢")
+
+# Using Polymorphism
+vehicles = [Car(), Plane(), Boat()]
+
+for v in vehicles:
+    v.move()
